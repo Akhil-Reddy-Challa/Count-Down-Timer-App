47c2b5b53de93397cce946096f47dbb01559ac95
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-11-01 22:19:38 +0530

    refined Notifications.js

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -2,27 +2,12 @@ export function checkNotificationPermissions() {
   const status = Notification.permission;
   if (status === "default") {
     requestPermission();
-  } else return;
+  }
 }
 function requestPermission() {
-  try {
-    Notification.requestPermission(function (result) {
-      if (result !== "granted") {
-        return false;
-      } else return true;
-    });
-  } catch (error) {
-    // Safari doesn't return a promise for requestPermissions and it
-    // throws a TypeError. It takes a callback as the first argument
-    // instead.
-    if (error instanceof TypeError) {
-      Notification.requestPermission((result) => {
-        if (result !== "granted") {
-          return false;
-        } else return true;
-      });
-    } else {
-      throw error;
+  Notification.requestPermission().then(function (result) {
+    if (result !== "granted") {
+      return;
     }
-  }
+  });
 }
